e8e5e8d45afbd3b91ab579078d12ba2409af90a6
victoroliveiramarquez <v13.oliveira.m@gmail.com>
2022-12-15 17:33:53 +0100

    subida

---
+++ EntornoClienteExamen/FelizNavidad/navidad.js
@@ -0,0 +1,8 @@
+window.onload;
+setTimeout(ver, 3000);
+
+function ver(){
+    document.getElementById("imagen").style.visibility="hidden";
+    document.getElementById("saludo").style.visibility="visible";
+    document.getElementById("cajaTexto").style.visibility="visible";
+}
